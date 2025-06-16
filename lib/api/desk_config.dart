95eb7652c4576059eaa29d5cfbc336aa954c84c9
shuisheng <2274751790@qq.com>
2025-06-16 13:30:56 +0800

    docs: 更新文档

--- lib/api/desk_config.dart
+++
@@ -1,8 +0,0 @@
-import 'package:billd_desk_flutter_pro/utils/request.dart';
-
-class DeskConfigApi {
-  static fetchDeskConfigTurnserver() async {
-    var res = await HttpRequest.get('/desk_config/turnserver');
-    return res;
-  }
-}
